6d8aaad03c1323f32c6ab1b9cf86bffb4cdef4b8
Fayis Rahman K <fayisrahman95@gmail.com>
2025-07-29 22:45:17 +0400

    design changes

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,14 @@
+@@ .. @@
+ import { MatTabsModule } from '@angular/material/tabs';
+ import { MatDatepickerModule } from '@angular/material/datepicker';
+ import { MatNativeDateModule } from '@angular/material/core';
++import { MatTooltipModule } from '@angular/material/tooltip';
+
+ const MaterialModules = [
+@@ .. @@
+   MatTabsModule,
+   MatDatepickerModule,
+-  MatNativeDateModule
++  MatNativeDateModule,
++  MatTooltipModule
+ ];
